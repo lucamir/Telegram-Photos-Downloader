f5ecbf196bd1b3b22fb5aaecf65025d887e6f429
Luca Mirabella <mirabella.luca@outlook.com>
2022-05-06 02:01:06 +0200

    Refactor
    refactor + add the terminal input for API ID and HASH

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ import telethon.sync
 from telethon import TelegramClient
 from telethon.tl.types import InputMessagesFilterPhotos
 
-api_id = 34423  # REPLACE THE EXAMPLE NUMBER WITH YOUR API ID CODE
-api_hash = ""  # PUT_YOUR_API_HASH_CODE_HERE
-session_name = "download_media"  # YOU CAN REPLACE THIS STRING WITH A CUSTOM SESSION NAME
-download_folder_path = "./downloaded-media"  # YOU CAN REPLACE THIS STRING WITH A CUSTOM FOLDER PATH
+SESSION_NAME = "download_media"  # YOU CAN REPLACE THIS STRING WITH A CUSTOM SESSION NAME
+OUTPUT_FOLDER = "./downloaded-media"  # YOU CAN REPLACE THIS STRING WITH A CUSTOM FOLDER PATH
 
 
 def find_chat(chats, chat_name):
@@ -18,9 +16,9 @@ def find_chat(chats, chat_name):
 
 
 def input_chat_name(chats):
-    print("\nWrite the chat name from which you want to downlaod the media or exit for close the program")
+    print("\nWrite the chat name from which you want to downlaod the media or exit for close the program\n")
     while True:
-        chat_name = input("What's the chat name? @> ")
+        chat_name = input("Please enter the chat name: ")
         if chat_name == "exit":
             quit()
         else:
@@ -37,37 +35,42 @@ def input_chat_name(chats):
 
 
 def main():
+    print("\nI need some info for create the connection...\n")
 
-    # creation of a client and start the session with your personal API ID AND HASH CODE
-    with TelegramClient(session_name, api_id, api_hash) as client:
+    try:
+        API_ID = int(input("Please enter your API ID: "))
+        API_HASH = input("Please enter your API HASH: ")
+        # creation of a client and start the session with your personal API ID AND HASH CODE
+        with TelegramClient(SESSION_NAME, API_ID, API_HASH) as client:
+            client.start()  # start the client
+            chats = client.get_dialogs()  # get all the conversations
 
-        client.start()  # start the client
+            # name of the conversation from which you want to download the photos
+            chat_name = input_chat_name(chats)
 
-        chats = client.get_dialogs()  # get all the conversations
+            chat = find_chat(chats, chat_name)  # find the selected chat
 
-        # name of the conversation from which you want to download the photos
-        chat_name = input_chat_name(chats)
+            if not path.exists(OUTPUT_FOLDER):
+                mkdir(OUTPUT_FOLDER)
 
-        chat = find_chat(chats, chat_name)  # find the selected chat
+            downlaod_counter = 0
 
-        if not path.exists(download_folder_path):
-            mkdir(download_folder_path)
+            # loop for get from all the messages of the selected conversation just the message that contains photos
+            for message in client.iter_messages(chat, filter=InputMessagesFilterPhotos):
+                if message.media:
+                    downlaod_counter += 1
+                    print(f"Downloaded media: {downlaod_counter}", end="\r")
+                    client.download_media(message, OUTPUT_FOLDER)
+                    sleep(0.25)
+            print(f"Downloaded media: {downlaod_counter}")
 
-        downlaod_counter = 0
+        # removing the session info from the local folder
+        session_path = f"./{SESSION_NAME}.session"
+        if path.exists(session_path):
+            remove(session_path)
 
-        # loop for get from all the messages of the selected conversation just the message that contains photos
-        for message in client.iter_messages(chat, filter=InputMessagesFilterPhotos):
-            if message.media:
-                downlaod_counter += 1
-                print(f"Downloaded media: {downlaod_counter}", end="\r")
-                client.download_media(message, download_folder_path)
-                sleep(0.25)
-        print(f"Downloaded media: {downlaod_counter}")
-
-    # removing the session info from the local folder
-    session_path = f"./{session_name}.session"
-    if path.exists(session_path):
-        remove(session_path)
+    except Exception as e:
+        print(f"[Error]: {e}")
 
 
 if __name__ == "__main__":
